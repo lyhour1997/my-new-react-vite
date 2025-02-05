13757a2f2fb4998637f2f6d99a7d18d0165bb6a1
lyhour1997/New-project.git <chlyhour168@gmail.com>
2025-02-05 13:05:01 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import BootStrap from './components/BootStrap'
+
+function App() {
+
+  return (
+    <>
+      <header>
+          <BootStrap />
+      </header>
+    </>
+  )
+
+}
+
+export default App;
---
+++ src/components/BootStrap.jsx
@@ -0,0 +1,51 @@
+
+import React from 'react'
+
+const BootStrap = () => {
+    return (
+        <div className='container '>
+            <div className="row bg-light py-2 ">
+                <div className="col-3">
+                    <h1>LOGO</h1>
+                </div>
+                <div className="col-6">
+                    <form className='form-control d-flex border-0 rounded-0 mt-2 p-0'>
+                        <input type="text" className='form-control shadow-none border rounded-0'/>
+                        <button>Search</button>
+                    </form>
+                </div>
+                <div className="col-3 d-flex align-items-center justify-content-end">
+                    <div className="">
+                        <button className=' btn rounded-0 border shadow-none bg-success text-light'>Register</button>
+                        <button className=' btn rounded-0 border shadow-none bg-danger text-light'>Login</button>
+                    </div>
+                </div>
+            </div>
+
+            <div className="row mt-3 ">
+                <div className="col-3">
+                    <div style={{ height:300}} className=" border">
+                        <img src="https://as2.ftcdn.net/v2/jpg/06/84/67/69/1000_F_684676995_qMQYQbIq90xVvfdWs10Y6mewyfbSDkKB.jpg" alt="" className='w-100 h-100 object-fit-cover' />    
+                    </div> 
+                </div>
+                <div className="col-3">
+                    <div style={{ height:300}} className=" border">
+                        <img src="https://as2.ftcdn.net/v2/jpg/06/84/67/69/1000_F_684676995_qMQYQbIq90xVvfdWs10Y6mewyfbSDkKB.jpg" alt="" className='w-100 h-100 object-fit-cover' />    
+                    </div> 
+                </div>
+                <div className="col-3">
+                    <div style={{ height:300}} className=" border">
+                        <img src="https://as2.ftcdn.net/v2/jpg/06/84/67/69/1000_F_684676995_qMQYQbIq90xVvfdWs10Y6mewyfbSDkKB.jpg" alt="" className='w-100 h-100 object-fit-cover' />    
+                    </div> 
+                </div>
+                <div className="col-3">
+                    <div style={{ height:300}} className=" border">
+                        <img src="https://as2.ftcdn.net/v2/jpg/06/84/67/69/1000_F_684676995_qMQYQbIq90xVvfdWs10Y6mewyfbSDkKB.jpg" alt="" className='w-100 h-100 object-fit-cover' />    
+                    </div> 
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default BootStrap
