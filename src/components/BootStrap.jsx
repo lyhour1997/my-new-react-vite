8d9ebb1b87f1dfdb9c92eb14390d81f781af105e
lyhour1997/New-project.git <chlyhour168@gmail.com>
2025-02-05 23:44:21 +0700

    update

--- src/components/BootStrap.jsx
+++ src/components/BootStrap.jsx
@@ -3,71 +3,73 @@ import React from 'react'
 
 const BootStrap = () => {
     return (
-        <div className='container '>
-            <div className="row bg-light py-2 ">
+      <div className="container-fluid">
+        <div style={{ marginTop:"35px"}} className='container p-md-0 bg-success text-light position-fixed top-0 start-50 translate-middle'>
+            <div className="row py-2 ">
                 <div className="col-3">
                     <h1>LOGO</h1>
                 </div>
-                <div className="col-6">
-                    <form className='form-control d-flex border-0 rounded-0 mt-2 p-0'>
-                        <input type="text" className='form-control shadow-none border rounded-0'/>
-                        <button>Search</button>
+                <div className="col-md-6 col-9 mt-md-2">
+                    <form className='form-control d-flex border-0 rounded-0 p-0'>
+                        <input type="text" placeholder='Search product' className='form-control shadow-none border rounded-0'/>
+                        <button className=' btn rounded-0 border'>Search</button>
                     </form>
                 </div>
-                <div className="col-3 d-flex align-items-center justify-content-end">
-                    <div className="">
+                <div className="col-3 d-none d-md-block d-flex align-items-center justify-content-end">
+                    <div className="my-2 text-end">
                         <button className=' btn rounded-0 border shadow-none bg-success text-light'>Register</button>
                         <button className=' btn rounded-0 border shadow-none bg-danger text-light'>Login</button>
                     </div>
                 </div>
             </div>
-
-            <div className="row mt-3 ">
-                <div className="col-3">
-                    <div style={{ height:300}} className=" border">
-                        <img src="https://as2.ftcdn.net/v2/jpg/06/84/67/69/1000_F_684676995_qMQYQbIq90xVvfdWs10Y6mewyfbSDkKB.jpg" alt="" className='w-100 h-100 object-fit-cover' />    
-                    </div> 
-                </div>
-                <div className="col-3">
-                    <div style={{ height:300}} className=" border">
-                        <img src="https://as2.ftcdn.net/v2/jpg/06/84/67/69/1000_F_684676995_qMQYQbIq90xVvfdWs10Y6mewyfbSDkKB.jpg" alt="" className='w-100 h-100 object-fit-cover' />    
-                    </div> 
-                </div>
-                <div className="col-3">
-                    <div style={{ height:300}} className=" border">
-                        <img src="https://as2.ftcdn.net/v2/jpg/06/84/67/69/1000_F_684676995_qMQYQbIq90xVvfdWs10Y6mewyfbSDkKB.jpg" alt="" className='w-100 h-100 object-fit-cover' />    
-                    </div> 
-                </div>
-                <div className="col-3">
-                    <div style={{ height:300}} className=" border">
-                        <img src="https://as2.ftcdn.net/v2/jpg/06/84/67/69/1000_F_684676995_qMQYQbIq90xVvfdWs10Y6mewyfbSDkKB.jpg" alt="" className='w-100 h-100 object-fit-cover' />    
-                    </div> 
-                </div>
-            </div>
-
-            <div className="row mt-3 ">
-                <div className="col-3">
-                    <div style={{ height:200}} className="bg-danger">
-                        <img className=' w-100 h-100 object-fit-cover' src="https://www.91-cdn.com/hub/wp-content/uploads/2023/11/Best-gaming-phones-in-the-world.png" alt="" />
-                    </div>
-                </div>
-                <div className="col-3">
-                    <div style={{ height:200}} className="bg-danger">
-                        <img className=' w-100 h-100 object-fit-cover' src="https://www.91-cdn.com/hub/wp-content/uploads/2023/11/Best-gaming-phones-in-the-world.png" alt="" />
-                    </div>
-                </div>
-                <div className="col-3">
-                    <div style={{ height:200}} className="bg-danger">
-                        <img className=' w-100 h-100 object-fit-cover' src="https://www.91-cdn.com/hub/wp-content/uploads/2023/11/Best-gaming-phones-in-the-world.png" alt="" />
-                    </div>
-                </div>
-                <div className="col-3">
-                    <div style={{ height:200}} className="bg-danger">
-                        <img className=' w-100 h-100 object-fit-cover' src="https://www.91-cdn.com/hub/wp-content/uploads/2023/11/Best-gaming-phones-in-the-world.png" alt="" />
-                    </div>
-                </div>
-            </div>
         </div>
+        <div style={{ marginTop:"10%"}} className="container p-0 ">
+             <div className="row">
+                <div className=" col-lg-3 col-md-6  py-2">
+                    <div style={{ height:300}} className=" border">
+                        <img src="https://as2.ftcdn.net/v2/jpg/06/84/67/69/1000_F_684676995_qMQYQbIq90xVvfdWs10Y6mewyfbSDkKB.jpg" alt="" className='w-100 h-100 object-fit-cover' />    
+                    </div> 
+                </div>
+                <div className=" col-lg-3 col-md-6  py-2">
+                    <div style={{ height:300}} className=" border">
+                        <img src="https://as2.ftcdn.net/v2/jpg/06/84/67/69/1000_F_684676995_qMQYQbIq90xVvfdWs10Y6mewyfbSDkKB.jpg" alt="" className='w-100 h-100 object-fit-cover' />    
+                    </div> 
+                </div>
+                <div className=" col-lg-3 col-md-6  py-2">
+                    <div style={{ height:300}} className=" border">
+                        <img src="https://as2.ftcdn.net/v2/jpg/06/84/67/69/1000_F_684676995_qMQYQbIq90xVvfdWs10Y6mewyfbSDkKB.jpg" alt="" className='w-100 h-100 object-fit-cover' />    
+                    </div> 
+                </div>
+                <div className=" col-lg-3 col-md-6  py-2">
+                    <div style={{ height:300}} className=" border">
+                        <img src="https://as2.ftcdn.net/v2/jpg/06/84/67/69/1000_F_684676995_qMQYQbIq90xVvfdWs10Y6mewyfbSDkKB.jpg" alt="" className='w-100 h-100 object-fit-cover' />    
+                    </div> 
+                </div>
+                <div className="col-lg-3 col-md-6 py-2">
+                    <div style={{ height:200}} className="bg-danger">
+                        <img className=' w-100 h-100 object-fit-cover' src="https://www.91-cdn.com/hub/wp-content/uploads/2023/11/Best-gaming-phones-in-the-world.png" alt="" />
+                    </div>
+                </div>
+                <div className="col-lg-3 col-md-6 py-2">
+                    <div style={{ height:200}} className="bg-danger">
+                        <img className=' w-100 h-100 object-fit-cover' src="https://www.91-cdn.com/hub/wp-content/uploads/2023/11/Best-gaming-phones-in-the-world.png" alt="" />
+                    </div>
+                </div>
+                <div className="col-lg-3 col-md-6 py-2">
+                    <div style={{ height:200}} className="bg-danger">
+                        <img className=' w-100 h-100 object-fit-cover' src="https://www.91-cdn.com/hub/wp-content/uploads/2023/11/Best-gaming-phones-in-the-world.png" alt="" />
+                    </div>
+                </div>
+                <div className="col-lg-3 col-md-6 py-2">
+                    <div style={{ height:200}} className="bg-danger">
+                        <img className=' w-100 h-100 object-fit-cover' src="https://www.91-cdn.com/hub/wp-content/uploads/2023/11/Best-gaming-phones-in-the-world.png" alt="" />
+                    </div>
+                </div>
+            </div>
+
+        
+        </div>
+      </div>
     )
 }
 
